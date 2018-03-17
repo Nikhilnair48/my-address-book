fec6ce66a796f20c2f1e88fbbc516fc793dd4e54
Nikhilnair48 <nikhil.nair48@gmail.com>
2018-03-17 11:08:41 -0400

    Initial commit: added add and list contact components. Will display static data initially to decide on a UI layout.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  contacts: string[] = ["nikhil", "nitish", "kalpesh"];
+  title: string ="My Address Book";
+  firstName: string = "";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterLink, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactAddComponent } from './contact-add/contact-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactAddComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'contact-list',
+        component: ContactListComponent
+      },
+      {
+        path: 'contact-add',
+        component: ContactAddComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+}
